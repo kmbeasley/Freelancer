b1b309f69affa69bef11933ac3f3f1f517cd909c
Kyler Beasley <kylerbeasley7@gmail.com>
2026-05-04 17:35:15 -0500

    Freelancer attempt

--- index.js
+++ index.js
@@ -10,3 +10,79 @@ const NAMES = ["Alice", "Bob", "Carol", "Dave", "Eve"];
 const OCCUPATIONS = ["Writer", "Teacher", "Programmer", "Designer", "Engineer"];
 const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
+
+// === State ===
+const freelancers = Array.from({ length: NUM_FREELANCERS }, makeFreelancer);
+const averageRate = getAverageRate();
+
+function makeFreelancer() {
+  const name = samples(NAMES);
+  const occupation = sample(OCCUPATIONS);
+  const rate =
+    PRICE_RANGE.min +
+    Math.floor(Math.random() * (PRICE_RANGE - PRICE_RANGE.min));
+
+  return { name, occupation, rate };
+}
+
+function getAverageRate() {
+  const total = freelancers.reduce(
+    (total, freelancer) => total + freelancer.rate,
+    0,
+  );
+  return total / freelancers.length;
+}
+function FreelancerRow({ name, occupation, rate }) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// ===Component===
+
+// We can destructure an object directly in the function's parameters!
+function FreelancerRow({ name, occupation, rate }) {
+  const $tr = document.createElement("tr");
+  $tr.innerHTML = `
+    <td>${name}</td>
+    <td>${occupation}</td>
+    <td>$${rate}</td>
+  `;
+  return $tr;
+}
+
+function FreelancerRows() {
+  const $tbody = document.createElement("tbody");
+  const $freelancers = freelancers.map(FreelancerRow);
+  $tbody.replaceChildren(...$freelancers);
+  return $tbody;
+}
+
+function AverageRate() {
+  const $p = document.createElement("p");
+
+  $p.textContent = `The average rate is $${averageRate.toFixed(2)}.`;
+  return $p;
+}
+
+// === Render ===
+function render() {
+  const $app = document.querySelector("#app");
+
+  $app.innerHTML = `
+    <h1>Freelancer Forum</h1>
+    <AverageRate></AverageRate>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Occupation</th>
+          <th>Rate</th>
+        </tr>
+      </thead>
+      <tbody id="FreelancerRows"></tbody>
+    </table>
+  `;
+  $app.querySelector("AverageRate").replaceWith(AverageRate());
+  $app.querySelector("#FreelancerRows").replaceWith(FreelancerRows());
+}
+
+render();
